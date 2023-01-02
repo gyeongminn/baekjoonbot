b6b9c9368b96f81182136a40c5e8ef71031c6ae5
Gyeongmin <lee_gyeongmin@naver.com>
2023-01-03 02:50:15 +0900

    update

--- debug/bot_test.py
+++ debug/bot_test.py
@@ -1,4 +1,28 @@
+from bs4 import BeautifulSoup
+import requests
+import json
 import discord
+
+# api : https://solvedac.github.io/unofficial-documentation/#/operations/getProblemById
+
+def get_data(problem):
+    url = 'https://solved.ac/api/v3/problem/show?problemId='+str(problem)
+    requestData = requests.get(url)
+    return json.loads(requestData.content)
+
+
+def get_icon(level):
+    return 'https://d2gd6pc034wcta.cloudfront.net/tier/'+str(level)+'.svg'
+
+
+def get_level(level):
+    levels = ['Unrated', 'Bronze V', 'Bronze IV', 'Bronze III', 'Bronze II', 'Bronze I', 
+              'Silver V', 'Silver IV', 'Silver III', 'Silver II', 'Silver I', 
+              'Gold V', 'Gold IV', 'Gold III', 'Gold II', 'Gold I','Platinum V', 
+              'Platinum IV', 'Platinum III', 'Platinum II', 'Platinum I', 
+              'Diamond V', 'Diamond IV', 'Diamond III', 'Diamond II', 'Diamond I', 
+              'Ruby V', 'Ruby IV', 'Ruby III', 'Ruby II', 'Ruby I']
+    return levels[int(level)]
 
 PREFIX = '/'
 TOKEN = 'MTA1NTc5MTg2MTU3MjMyNTQxNw.Gke9cZ.HV0xh3n-TdIhDuqs9R9kw3wqjhBoboIQ2AB5Ok'
@@ -21,8 +45,24 @@ async def on_message(message):
 
     if message.content.startswith(f'{PREFIX}백준'):
         url = str(message.content).split('/백준 ')[1]
+        problem = url.split('https://www.acmicpc.net/problem/')[1]
         await message.delete()
-        await message.channel.send('문제 링크 :'+url)
+
+        data = get_data(problem)
+        
+        tags = []
+        for t in data['tags']:
+            tags.append(t['displayNames'][0]['name'])
+        tags = ", ".join(tags)
+
+        embed = discord.Embed(
+            title="문제 링크", url="https://www.acmicpc.net/problem/"+problem)
+        embed.set_author(name=data['titleKo'], url="https://www.acmicpc.net/problem/" +
+                         problem, icon_url="https://im2.ezgif.com/tmp/ezgif-2-21a8b14550.png")
+        embed.add_field(name="문제 번호", value=data['problemId'], inline=True)
+        embed.add_field(name="난이도", value=get_level(data['level']), inline=True)
+        embed.add_field(name="유형", value=tags, inline=False)
+        await message.channel.send(embed=embed)
 
 
 try:
